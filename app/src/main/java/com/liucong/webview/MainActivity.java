1f4757a9aa4b70e076e7c8cf4a2af7d2b17f1f1a
liucong02 <liucong02@corp.netease.com>
2020-03-25 00:01:48 +0800

    add support for webview proxy, don't forget to: 1,config fiddler correctly 2,install fiddler certificate through browser

--- app/src/main/java/com/liucong/webview/MainActivity.java
+++ app/src/main/java/com/liucong/webview/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -22,6 +23,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         webview = findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.clearCache(true);
+        webview.getSettings().setAppCacheEnabled(false);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        WebviewSettingProxy.setProxyKKPlus(webview, "192.168.1.102", 8888, "android.app.Application");
         webview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -41,7 +46,7 @@ public class MainActivity extends Activity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                } else if (true && url.equals("https://bh3.mihoyo.com/gl/_nuxt/71aed5e15513f19b272c.js")) {
+                } else if (false && url.equals("https://bh3.mihoyo.com/gl/_nuxt/71aed5e15513f19b272c.js")) {
                     try {
                         Log.d(TAG, "shouldInterceptRequest: replace " + url);
                         return new WebResourceResponse("text/javascript", "utf-8", getBaseContext().getAssets().open("71aed5e15513f19b272c_es5.js"));
@@ -62,7 +67,7 @@ public class MainActivity extends Activity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }*/ else if (url.equals("https://yixiu.game.163.com/F4D89668CAB384A1/js/app/index.js")) {
+                }*/ else if (false && url.equals("https://yixiu.game.163.com/F4D89668CAB384A1/js/app/index.js")) {
                     try {
                         Log.d(TAG, "shouldInterceptRequest: replace " + url);
                         return new WebResourceResponse("text/javascript", "utf-8", getBaseContext().getAssets().open("F4D89668CAB384A1_js_app_index_es5.js"));
@@ -96,8 +101,8 @@ public class MainActivity extends Activity {
         Log.d(TAG, "button: start load....");
         //webview.loadUrl("file:////android_asset/index.html");
         //webview.loadUrl("https://www.baidu.com/");
-        //webview.loadUrl("https://bh3.mihoyo.com/gl/character/detail/70?fullscreen=1&sign_type=2&authkey_ver=1&auth_appid=gl0429&authkey=A%2Fu%2FqMzOo25h4uxjT6HNUucRJoBB4j61HQ7EhExwRvL8bASgS3iC9JePgF0nKcQbzFbG899qJ99d%2F5z9s1H%2BO3tYaxC0fYrk1ZsRu0yV8xONrsxwnAg2JpRph5oIVcZJIpAoTMh1ku2GcNQnJzs4gjW7GeTcMyg2B9YI%2FKDObNYzXwHUTJAoD0QDYRzQXz3b3wIH5k3SqRIDatIMrMWsnsa625Ox1D1he5CM2jjaPbP5Qfj9Q3OwantlwnN6E2WiEvufiAmWPPSEIeS2nvqU7SwShqAUrnhYqt4nAyBwY%2Bpl0pLJrcmQDs8duWhPMZePSCMvBcoDuamhPsnpLRj7LynL11qhDqqcnJE%2FbKI%2B1JM7Ko6vCpbmu5axENJQEm2Cr7kPx%2BluiFucVwvWEwA5IdssKiAztTTAWzNllH3QVm13XmZ1dwgnB1E0sAGCEaeEObO7G5CjAU3Zh6QuUsJ10sfXkTP2focqJN5KIB4ZMrvH3os51sDhpZ2dMxRNH0e7mV%2F128f2dOXenfNJrQnsRuQQFenGVblzyqQLkxn4Axd%2Bo7ujjdQNEEwBmAwWeuONoQvyBA1m2OqeW3RJvtW%2BrKX9V3iORbMwvPNternWBJr0IoBiL3MQhDHIrXYIdb%2BczmjFKIobKQPBZEcGYgiUK2kD8AbWX3VMNQG21bjtsGs%3D&lang=zh-CN");
-        webview.loadUrl("http://mumu.163.com/mhome.html");
+        webview.loadUrl("https://bh3.mihoyo.com/gl/character/detail/70?fullscreen=1&sign_type=2&authkey_ver=1&auth_appid=gl0429&authkey=A%2Fu%2FqMzOo25h4uxjT6HNUucRJoBB4j61HQ7EhExwRvL8bASgS3iC9JePgF0nKcQbzFbG899qJ99d%2F5z9s1H%2BO3tYaxC0fYrk1ZsRu0yV8xONrsxwnAg2JpRph5oIVcZJIpAoTMh1ku2GcNQnJzs4gjW7GeTcMyg2B9YI%2FKDObNYzXwHUTJAoD0QDYRzQXz3b3wIH5k3SqRIDatIMrMWsnsa625Ox1D1he5CM2jjaPbP5Qfj9Q3OwantlwnN6E2WiEvufiAmWPPSEIeS2nvqU7SwShqAUrnhYqt4nAyBwY%2Bpl0pLJrcmQDs8duWhPMZePSCMvBcoDuamhPsnpLRj7LynL11qhDqqcnJE%2FbKI%2B1JM7Ko6vCpbmu5axENJQEm2Cr7kPx%2BluiFucVwvWEwA5IdssKiAztTTAWzNllH3QVm13XmZ1dwgnB1E0sAGCEaeEObO7G5CjAU3Zh6QuUsJ10sfXkTP2focqJN5KIB4ZMrvH3os51sDhpZ2dMxRNH0e7mV%2F128f2dOXenfNJrQnsRuQQFenGVblzyqQLkxn4Axd%2Bo7ujjdQNEEwBmAwWeuONoQvyBA1m2OqeW3RJvtW%2BrKX9V3iORbMwvPNternWBJr0IoBiL3MQhDHIrXYIdb%2BczmjFKIobKQPBZEcGYgiUK2kD8AbWX3VMNQG21bjtsGs%3D&lang=zh-CN");
+        //webview.loadUrl("http://mumu.163.com/mhome.html");
         Log.d(TAG, "button: end load....");
     }
 }
---
+++ app/src/main/java/com/liucong/webview/WebviewSettingProxy.java
@@ -0,0 +1,165 @@
+package com.liucong.webview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Proxy;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.webkit.WebView;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class WebviewSettingProxy {
+
+    private static final String LOG_TAG = "liucong";
+
+
+    // from https://stackoverflow.com/questions/19979578/android-webview-set-proxy-programatically-kitkat
+    @SuppressLint("NewApi")
+    @SuppressWarnings("all")
+    public static boolean setProxyKKPlus(WebView webView, String host, int port, String applicationClassName) {
+        Log.d(LOG_TAG, "Setting proxy with >= 4.4 API.");
+        Context appContext = webView.getContext().getApplicationContext();
+        System.setProperty("http.proxyHost", host);
+        System.setProperty("http.proxyPort", port + "");
+        System.setProperty("https.proxyHost", host);
+        System.setProperty("https.proxyPort", port + "");
+        try {
+            Class applictionCls = Class.forName(applicationClassName);
+            Field loadedApkField = applictionCls.getField("mLoadedApk");
+            loadedApkField.setAccessible(true);
+            Object loadedApk = loadedApkField.get(appContext);
+            Class loadedApkCls = Class.forName("android.app.LoadedApk");
+            Field receiversField = loadedApkCls.getDeclaredField("mReceivers");
+            receiversField.setAccessible(true);
+            ArrayMap receivers = (ArrayMap) receiversField.get(loadedApk);
+            for (Object receiverMap : receivers.values()) {
+                for (Object rec : ((ArrayMap) receiverMap).keySet()) {
+                    Class clazz = rec.getClass();
+                    if (clazz.getName().contains("ProxyChangeListener")) {
+                        Method onReceiveMethod = clazz.getDeclaredMethod("onReceive", Context.class, Intent.class);
+                        Intent intent = new Intent(Proxy.PROXY_CHANGE_ACTION);
+                        onReceiveMethod.invoke(rec, appContext, intent);
+                    }
+                }
+            }
+            Log.d(LOG_TAG, "Setting proxy with >= 4.4 API successful!");
+            return true;
+        } catch (ClassNotFoundException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (NoSuchFieldException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (IllegalAccessException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (IllegalArgumentException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (NoSuchMethodException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (InvocationTargetException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        }
+        return false;
+    }
+
+    @SuppressLint("NewApi")
+    @SuppressWarnings("all")
+    public static boolean revertProxyKKPlus(WebView webView, String applicationClassName) {
+        Context appContext = webView.getContext().getApplicationContext();
+        Properties properties = System.getProperties();
+        properties.remove("http.proxyHost");
+        properties.remove("http.proxyPort");
+        properties.remove("https.proxyHost");
+        properties.remove("https.proxyPort");
+        try {
+            Class applictionCls = Class.forName(applicationClassName);
+            Field loadedApkField = applictionCls.getField("mLoadedApk");
+            loadedApkField.setAccessible(true);
+            Object loadedApk = loadedApkField.get(appContext);
+            Class loadedApkCls = Class.forName("android.app.LoadedApk");
+            Field receiversField = loadedApkCls.getDeclaredField("mReceivers");
+            receiversField.setAccessible(true);
+            ArrayMap receivers = (ArrayMap) receiversField.get(loadedApk);
+            for (Object receiverMap : receivers.values()) {
+                for (Object rec : ((ArrayMap) receiverMap).keySet()) {
+                    Class clazz = rec.getClass();
+                    if (clazz.getName().contains("ProxyChangeListener")) {
+                        Method onReceiveMethod = clazz.getDeclaredMethod("onReceive", Context.class, Intent.class);
+                        Intent intent = new Intent(Proxy.PROXY_CHANGE_ACTION);
+//                        intent.putExtra("proxy", null);
+                        onReceiveMethod.invoke(rec, appContext, intent);
+                    }
+                }
+            }
+            Log.d(LOG_TAG, "Revert proxy with >= 4.4 API successful!");
+            return true;
+        } catch (ClassNotFoundException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (NoSuchFieldException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (IllegalAccessException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (IllegalArgumentException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (NoSuchMethodException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        } catch (InvocationTargetException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            Log.v(LOG_TAG, e.getMessage());
+            Log.v(LOG_TAG, exceptionAsString);
+        }
+        return false;
+    }
+
+}
